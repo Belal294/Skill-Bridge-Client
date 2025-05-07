cff1bce2ef2ebd866b1d5bb9e1aefba24e70db99
Belal Islam <belalislam294@gmail.com>
2025-05-08 00:25:39 +0600

    fix bugs again

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
 // src/pages/Home.jsx
 import Features from '../components/Features';
-import HeroCarousel from '../components/carousel/HeroCarousel';
 import Services from '../../src/Serice-Product/Product';
 import DiscountSection from '../Discount/DiscountSection';
 import Category from '../components/categories/Category';
 import SectionOne from '../components/Carousel/SectionOne'
 import SectionTwo from '../components/Carousel/SectionTwo';
 import ContactForm from '../layouts/ContactForm';
+import HeroCarousel from '../components/carousel/HeroCarousel';
 
 const Home = () => {
   return (
