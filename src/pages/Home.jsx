7051274d5ff7ec35b14d8448ca7dd11dadd373a9
Belal Islam <belalislam294@gmail.com>
2025-05-08 21:38:34 +0600

    Just Check

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,24 @@
-// src/pages/Home.jsx
+
 import Features from '../components/Features';
-import Services from '../../src/Serice-Product/Product';
+import Services from '../Serice-Product/Product';
 import DiscountSection from '../Discount/DiscountSection';
 import Category from '../components/categories/Category';
-import SectionOne from '../components/Carousel/SectionOne'
+import SectionOne from '../components/Carousel/SectionOne';
 import SectionTwo from '../components/Carousel/SectionTwo';
 import ContactForm from '../layouts/ContactForm';
-import HeroCarousel from '../components/carousel/HeroCarousel';
+import HeroCarousel from '../components/Carousel/HeroCarousel';
 
 const Home = () => {
   return (
     <div>
-      <HeroCarousel/>
-      <Features/>
-      <SectionOne/>
-      <Category/>
-      <Services/>
-      <SectionTwo/>
-      <DiscountSection/>
-      <ContactForm/>
-        
+      <HeroCarousel />
+      <Features />
+      <SectionOne />
+      <Category />
+      <Services />
+      <SectionTwo />
+      <DiscountSection />
+      <ContactForm />
     </div>
   );
 };
