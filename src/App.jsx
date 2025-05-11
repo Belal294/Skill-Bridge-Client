82ae67807eb62899fd79bc6cebab4558e7c2c44b
Belal Islam <belalislam294@gmail.com>
2025-05-11 23:57:40 +0600

    add hash

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-// App.jsx
 import './App.css';
 import { AuthProvider } from './context/AuthContext';
 import AppRoutes from './routes/AppRoutes';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom'; 
 import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <AuthProvider>
-    <BrowserRouter>
-    <Toaster position="top-right" />
-      <AppRoutes />
-    </BrowserRouter>
+      <HashRouter>
+        <Toaster position="top-right" />
+        <AppRoutes />
+      </HashRouter>
     </AuthProvider>
   );
 }
