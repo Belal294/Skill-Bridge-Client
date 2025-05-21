59a3d910504c7d9cb99c7de66b5f9e80106bb0ba
Belal Islam <belalislam294@gmail.com>
2025-05-21 13:04:11 +0600

    ready for deploy

--- src/services/api-client.js
+++ src/services/api-client.js
@@ -1,11 +1,12 @@
-// import axios from "axios";
-
-// export default axios.create({
-//   baseURL: "https://skill-bridge-silk.vercel.app/api/v1",
-// });
-
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://127.0.0.1:8000/api/v1",
+  baseURL: "https://skill-bridge-ashen.vercel.app/api/v1",
 });
+
+
+// import axios from "axios";
+
+// export default axios.create({
+//   baseURL: "http://127.0.0.1:8000/api/v1",
+// });
