410fdd3720edf9b68505e0d0c166b40dea08b32b
Belal Islam <belalislam294@gmail.com>
2025-05-22 17:41:52 +0600

    build setup

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   server: {
-    historyApiFallback: true,
     proxy: {
       '/api': {
         target: 'https://skill-bridge-one.vercel.app',
@@ -16,5 +15,6 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-  }
+  },
+  base: './', // for relative asset paths
 })
